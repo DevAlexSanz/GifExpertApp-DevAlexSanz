c707467b3017bb18ca48d823951f66aeba2b0181
DevAlexSanz <alexsanz020418@outlook.com>
2023-02-25 22:49:57 -0600

    commit

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=kaJ1JwD4CuQgYun7YpTQpTr5p1qs1sQn&q=${category}&limit=10`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=R57QLoIrCHmxROf9CJAgf7pv4HyJVH8O&q=${category}&limit=10`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
